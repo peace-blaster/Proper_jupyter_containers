b1d6162df42a06b32234344734f724e96201200a
Julian DeVille <53p3n7@gmail.com>
2020-12-08 01:24:27 -0500

    Create deploy.sh
    bash script for deploying temporary Jupyter container with persistent storage

---
+++ deploy.sh
@@ -0,0 +1,1 @@
+podman run -p 8888:8888 -e JUPYTER_TOKEN=yoursecurepassword -e NB_UID=1000 -e CHOWN_EXTRA="/home/jovyan/work" --user root -v /home/USER/jupyter_stuff:/home/jovyan/work jupyter/pyspark-notebook
